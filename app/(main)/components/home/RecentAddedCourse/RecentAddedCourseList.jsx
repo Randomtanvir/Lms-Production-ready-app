79461d7d092b02a3e58de9683e085c6ead75f654
Tanvir Ahmad <tanvir.info07@gmail.com>
2025-04-13 11:26:24 +0600

    fix margin

--- app/(main)/components/home/RecentAddedCourse/RecentAddedCourseList.jsx
+++ app/(main)/components/home/RecentAddedCourse/RecentAddedCourseList.jsx
@@ -3,7 +3,7 @@ import CourseCard from "./CourseCard";
 export default async function CourseList({ courses }) {
   const RecentAddedCourses = courses.slice(0, 4);
   return (
-    <div className="bg-white mt-32 sm:mt-40 lg:mt-60 py-20 w-full">
+    <div className="bg-white py-20 w-full">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <h2 className="text-3xl sm:text-4xl font-semibold text-center mb-10">
           Recently added courses
